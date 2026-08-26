8ee3fcff362eafb448e997a6de615e8135e5d8a4
rafajcc <rafajcc@gmail.com>
2026-08-26 12:52:56 +0200

    Bump version to 3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.fretpitch"
         minSdk = 33
         targetSdk = 34
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "3.1"
     }
 
     buildTypes {
@@ -41,7 +41,7 @@ android {
     applicationVariants.all {
         outputs.all {
             val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
-            output.outputFileName = "fretpitch_v3.apk"
+            output.outputFileName = "fretpitch_v3.1.apk"
         }
     }
 }
